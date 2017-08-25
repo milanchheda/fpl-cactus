74aef7df53dd1c00d171326f62d3c652a5be17e3
milanchheda <milan.chheda@gmail.com>
2017-08-25 14:12:27 +0400

    Added club logos

--- resources/views/fixtures.blade.php
+++ resources/views/fixtures.blade.php
@@ -12,12 +12,20 @@
 			@foreach($allFixtures as $fixture)
 				<tr data-fixture-id="{{ $fixture->id }}" id="team-{{ $fixture->id }}" data-winning-team-id="{{ $fixture->winning_team_id }}">
 					<td id="{{ $fixture->home_team_id }}" class="col-md-5 text-right">
-					{{ $fixture->home_team }}
+						<span class="club {{ $fixture->home_team }}">
+							<span class="teamName">
+								{{ $fixture->home_team }}
+							</span>
+						</span>
 					</td>
-						<td class="col-md-1 text-center">{{ $fixture->team_home_score }}</td>
-						<td class="col-md-1 text-center">{{ $fixture->team_away_score }}</td>
+						<td class="col-md-1 text-center scoreClass">{{ $fixture->team_home_score }}</td>
+						<td class="col-md-1 text-center scoreClass">{{ $fixture->team_away_score }}</td>
 					<td id="{{ $fixture->away_team_id }}" class="col-md-5 text-left">
-					{{ $fixture->away_team }}
+						<span class="club {{ $fixture->away_team }}">
+							<span class="teamName righTeamName">
+							{{ $fixture->away_team }}
+							</span>
+						</span>
 					</td>
 					<td class="user-bet"></td>
 				</tr>
