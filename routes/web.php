4a37ba5c09470a1d48f865a03d6e04c8ebecc106
milanchheda <milan.chheda@gmail.com>
2017-09-08 18:06:17 +0400

    Minor route change

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/chat', function () {
 
 Auth::routes();
 
-// Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'FplPlayersController@index');
 Route::get('/give-bets', ['middleware' => 'auth', 'uses' => 'BetsController@getBets']);
 Route::get('/stats', ['middleware' => 'auth', 'uses' => 'HomeController@index']);
